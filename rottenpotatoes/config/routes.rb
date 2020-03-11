998e30e31b8c03d65f5449f1fd531525c3e2ef83
EC2 Default User <ec2-user@ip-172-31-29-65.us-east-2.compute.internal>
2020-03-11 01:59:42 +0000

    tried

--- rottenpotatoes/config/routes.rb
+++ rottenpotatoes/config/routes.rb
@@ -2,4 +2,5 @@ Rottenpotatoes::Application.routes.draw do
   resources :movies
   # map '/' to be a redirect to '/movies'
   root :to => redirect('/movies')
+  get '/movies/:id/all_test', to: 'movies#same_director', as: 'find_similar_router'
 end
